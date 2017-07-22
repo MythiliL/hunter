3dba496678a2127cd4304d654ec3aa5916d434ce
MythiliL <mythili007.07@gmail.com>
2017-07-22 11:59:56 +0530

    Create Leapyr.java

---
+++ Leapyr.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class Leapyr
+{
+    public static void main(String args[])
+    {
+        int a;
+        Scanner scan=new Scanner(System.in);
+        a=scan.nextInt();
+        if(a%4==0)
+       
+        System.out.print("leap year:"+a);
+        else
+        System.out.print("not a leap year");
+     }
+ }
